d9b4d91e2a5031f9a29d74376824bbcb7f79b7ab
algorithmlx <izhukov2007@gmail.com>
2024-05-15 17:08:20 +0700

    gradle updates *_*

--- neoforge/build.gradle.kts
+++ neoforge/build.gradle.kts
@@ -60,9 +60,11 @@ publishing {
     repositories {
         val mk = System.getenv("MAVEN_KEY")
         val mp = System.getenv("MAVEN_PASS")
+        val releaseType: String by rootProject
+        val artefact = if (releaseType.isEmpty()) "releases" else "snapshots"
 
         if (mk != null && mp != null) {
-            maven("http://maven.0mods.team") {
+            maven("https://maven.0mods.team/$artefact/") {
                 credentials {
                     username = mk
                     password = mp
